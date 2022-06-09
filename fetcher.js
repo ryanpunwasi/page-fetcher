9fb0f0f1f073c6b0f8d489f92ee0d65400b2cd63
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-08 21:14:45 -0400

    Fixed linting errors

--- fetcher.js
+++ fetcher.js
@@ -3,15 +3,15 @@ const fs = require('fs');
 
 const args = process.argv.slice(2);
 request(args[0], (error, response, body) => {
-  if(!error && response.statusCode === 200) {
-  const content = body;
-  fs.writeFile(args[1], content, err => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(`Downloaded and saved ${body.length} bytes to ${args[1]}`);
-    }
-  });
+  if (!error && response.statusCode === 200) {
+    const content = body;
+    fs.writeFile(args[1], content, err => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(`Downloaded and saved ${body.length} bytes to ${args[1]}`);
+      }
+    });
   } else if (error.code === 'ENOTFOUND') {
     console.log("You entered an invalid url.");
   }
